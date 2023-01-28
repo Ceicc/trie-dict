992cb0f649888fc7133c7831f3a023f7d4270886
husam <husam999h@gmail.com>
2023-01-28 15:05:22 +0300

    The basic functionality is done, but still didn't figure out the correct type to add to the storage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-export default function TrieDict() {}
+type TrieDictConstructor = {
+	/**
+	 * The initial words to add the Trie
+	 */
+	words: string[];
+};
+
+type node = {
+	completeWord: boolean;
+	[k: string]: node;
+};
+
+export default function TrieDict({ words }: TrieDictConstructor) {
+	const storage: Record<string, node> = Object.create(null);
+
+	words.forEach(add);
+
+	function add(word: string) {
+		let pointer = storage;
+		// word.split("").forEach((char) => {
+		for (const char of word) {
+			if (Object.prototype.hasOwnProperty.call(pointer, char)) {
+				pointer = pointer[char];
+				continue;
+			}
+			pointer[char] = { completeWord: false };
+			pointer = pointer[char];
+		}
+		pointer.completeWord = true;
+	}
+
+	console.log(storage);
+
+	return {
+		has(word: string) {
+			let pointer = storage;
+			for (const char of word) {
+				if (!Object.prototype.hasOwnProperty.call(pointer, char)) {
+					return false;
+				}
+				pointer = pointer[char];
+			}
+			return pointer.completeWord;
+		},
+		add,
+	};
+}
